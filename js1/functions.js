b8026b07895b6b20a91153943fe8e62502641f28
Dumie1 <dumiedumara@gmail.com>
2018-09-15 00:52:56 +0200

    new code

---
+++ js1/functions.js
@@ -0,0 +1,18 @@
+/* -------Function Declarations---------
+
+function getReminder() {
+  console.log('Water the plants.');
+}
+function greetInSpanish() {
+  console.log('Buenas Tardes.');
+}
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+Calling a Function
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+sayThanks();
+sayThanks();*/
