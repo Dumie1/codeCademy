8b022730b3b98f93b3b675a5824b0f3f19b1d113
Dumie1 <dumiedumara@gmail.com>
2018-09-20 23:49:19 +0200

    added final code

--- js1/loops.js
+++ js1/loops.js
@@ -11,7 +11,116 @@ for (let counter = 3; counter >= 0; counter--){
   console.log(counter)
 };*/
 
-/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------Looping through Arrays--------
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit ' + vacationSpots[i]);
+}*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----Nested Loops-----
+
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};
+
+--------------------------------------------------
+
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray[i] === yourArray[j]) {
+      console.log('Both loops have the number: ' + myArray[i])
+    }
+  }
+};*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----The While Loop-----
+
+// A for loop that prints 1, 2, and 3
+for (let counterOne = 1; counterOne < 4; counterOne++){
+  console.log(counterOne)
+}
+
+// A while loop that prints 1, 2, and 3
+let counterTwo = 1;
+while (counterTwo < 4) {
+  console.log(counterTwo);
+  counterTwo++
+};
+----------------------------------------------------
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+	console.log(currentCard);
+}*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------Do...While Statements-----------
+
+let countString = '';
+let i = 0;
+do {
+  countString = countString + i;
+  i++;
+} while (i < 5);
+console.log(countString);
+
+--------------------------------------------------
+
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+do {
+ cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);*/
+
+/* *****************************************************
+-------The break Keyword-------
+
+for (let i = 0; i < 99; i++){
+  console.log('Banana.');
+  if (i > 3) {
+     break;
+  };
+};
+console.log('Orange you glad I broke out the loop!');
+
+--------------------------------------------------
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write you code below
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i])
+  if (rapperArray[i] ===  'Notorious B.I.G.'){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.")
+*/
+
+
+
+/*  More notes on loops
+
+*  one drawback of for loop is that you need a condition stating exactly how many times the loop should run
 
 
 */
