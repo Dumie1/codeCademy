a0b310ecb493eda707f1230a98ac372f4f677e84
Dumie1 <dumiedumara@gmail.com>
2018-09-21 21:18:11 +0200

    added iteration file

---
+++ js1/iterators.js
@@ -0,0 +1,127 @@
+/*
+The built-in JavaScript array methods that help us iterate are called iteration 
+methods, at times referred to as iterators. Iterators are methods called on arrays
+ to manipulate elements and return values.
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----Introduction to Iterators----
+
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+console.log(onlyNumbers);*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+.forEach() takes an argument of callback function.
+Callback function is a function passed as an argument into another function.
+-------forEach---------
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+When .map() is called on an array, it takes an argument of a callback function
+and returns a new array!
+------The .map() Method-------
+
+const numbers = [1, 2, 3, 4, 5];
+const bigNumbers = numbers.map(number => {
+  return number * 10;
+});
+-----------------------------------------------------
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+const secretMessage = animals.map(animal => animal[0]);
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(num => num/100);
+console.log(smallNumbers)*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+.filter() returns an array of elements after filtering out certain elements
+ from the original array. The callback function for the .filter() method 
+ should return true or false depending on the element that is passed to it.
+The elements cause the callback function to return true are added to the new array.
+-----The .filter() Method----
+
+const words = ['chair', 'music', 'pillow', 'brick', 'pen', 'door'];
+const shortWords = words.filter(word => {
+  return word.length < 6;
+});
+
+------------------------------------------
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250;
+})
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+const longFavoriteWords = favoriteWords.filter(word => {
+  return word.length > 7;
+})
+console.log(longFavoriteWords);*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+To find the location of an element in an array.  Calling .findIndex()
+on an array will return the index of the first element that evaluates 
+to true in the callback function
+--------The .findIndex() Method---------
+
+const jumbledNums = [123, 25, 78, 5, 9];
+const lessThanTen = jumbledNums.findIndex(num => {
+  return num < 10;
+});
+
+-----------------------------------------------
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+ The .reduce() method returns a single value after iterating through
+the elements of an array, thereby reducing the array.
+-------The .reduce() Method---------
+
+const numbers = [1, 2, 4, 10];
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+})
+console.log(summedNums) // Output: 17
+
+----------------------------------------------
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+console.log(newSum);*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+*/
