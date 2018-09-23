09b7d73085c232e50a41a00bf651efe74b029d49
Dumie1 <dumiedumara@gmail.com>
2018-09-23 16:15:44 +0200

    new code

---
+++ js1/arrayMethods.js
@@ -0,0 +1,20 @@
+
+/*======Array methods======
+
+Add / remove items
+
+arr.push(...items) – adds items to the end,
+arr.pop() – extracts an item from the end,
+arr.shift() – extracts an item from the beginning,
+arr.unshift(...items) – adds items to the beginning.
+*/
+
+/*
+In the next example we remove 3 elements
+and replace them with the other two:
+
+let arr = ["I", "study", "JavaScript", "right", "now"];
+// remove 3 first elements and replace them with another
+arr.splice(0, 3, "Let's", "dance");
+alert( arr ) // now ["Let's", "dance", "right", "now"]
+*/
