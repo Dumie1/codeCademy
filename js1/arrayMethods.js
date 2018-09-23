f2266e4cd5b7d60e781c0f1a8e122be67b1b5372
Dumie1 <dumiedumara@gmail.com>
2018-09-23 19:00:04 +0200

    more code

--- js1/arrayMethods.js
+++ js1/arrayMethods.js
@@ -9,7 +9,7 @@ arr.shift() – extracts an item from the beginning,
 arr.unshift(...items) – adds items to the beginning.
 */
 
-/*
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 In the next example we remove 3 elements
 and replace them with the other two:
 
@@ -18,3 +18,105 @@ let arr = ["I", "study", "JavaScript", "right", "now"];
 arr.splice(0, 3, "Let's", "dance");
 alert( arr ) // now ["Let's", "dance", "right", "now"]
 */
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+Here we can see that splice returns the array of removed elements:
+
+let arr = ["I", "study", "JavaScript", "right", "now"];
+// remove 2 first elements
+let removed = arr.splice(0, 2);
+alert( removed ); // "I", "study" <-- array of removed elements
+
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----Negative indexes allowed-----
+
+let arr = [1, 2, 5];
+// from index -1 (one step from the end)
+// delete 0 elements,
+// then insert 3 and 4
+arr.splice(-1, 0, 3, 4);
+alert( arr ); // 1,2,3,4,5
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----slice----
+It returns a new array where it copies all items start index "start" to "end" (not including "end")
+works like str.slice, but makes subarrays instead of substrings
+
+let str = "test";
+let arr = ["t", "e", "s", "t"];
+alert( str.slice(1, 3) ); // es
+alert( arr.slice(1, 3) ); // e,s
+alert( str.slice(-2) ); // st
+alert( arr.slice(-2) ); // s,t*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+------concat--------
+The method arr.concat joins the array with other arrays and/or items.
+
+let arr = [1, 2];
+
+// merge arr with [3,4]
+alert( arr.concat([3, 4])); // 1,2,3,4
+// merge arr with [3,4] and [5,6]
+alert( arr.concat([3, 4], [5, 6])); // 1,2,3,4,5,6
+// merge arr with [3,4], then add values 5 and 6
+alert( arr.concat([3, 4], 5, 6)); // 1,2,3,4,5,6
+*/
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+// Searching in array
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----indexOf/lastIndexOf and includes---
+
+arr.indexOf(item, from) looks for item starting from index from, and returns the index where it was found, otherwise -1.
+arr.lastIndexOf(item, from) – same, but looks from right to left.
+arr.includes(item, from) – looks for item starting from index from, returns true if found.
+
+let arr = [1, 0, false];
+
+alert( arr.indexOf(0) ); // 1
+alert( arr.indexOf(false) ); // 2
+alert( arr.indexOf(null) ); // -1
+alert( arr.includes(1) ); // true
+
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----find and findIndex---
+
+let users = [
+  {id: 1, name: "John"},
+  {id: 2, name: "Pete"},
+  {id: 3, name: "Mary"}
+];
+let user = users.find(item => item.id == 1);
+alert(user.name); // John
+*/
+
+// Transform an array
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------------
+let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length)
+alert(lengths); // 5,7,6
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----sort(fn)-----
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+let arr = [ 1, 2, 15 ];
+arr.sort(compareNumeric);
+alert(arr);  // 1, 2, 15
+*/
+
+// REFERENCES
+// http://javascript.info/array-methods
