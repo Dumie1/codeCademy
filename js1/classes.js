a4fec4a1b2af66cfba70f6c64aa8108e8a47f5cc
Dumie1 <dumiedumara@gmail.com>
2018-09-24 18:54:19 +0200

    update file, classes.js

--- js1/classes.js
+++ js1/classes.js
@@ -75,7 +75,101 @@ class Surgeon  {
 An instance is an object that contains the property names and methods of a class,
 but with unique property values
 
+class Dog {
+  constructor(name) {
+    this.name = name;
+    this.behavior = 0;
+  }
+}
+const halley = new Dog('Halley'); // Create new Dog instance
+console.log(halley.name); // Log the name value saved to halley
+// Output: 'Halley'
 
+------------------------------------------------------
+class Surgeon  {
+  constructor(name, department){
+    this.name = name;
+    this.department = department;
+  }
+}
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----------Methods----------
+Class method and getter syntax is the same as it is for objects except you can 
+not include commas between methods.
+
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+--------------------------------------------------------
+
+class Surgeon {
+  constructor(name, department, remainingVacationDays) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name(){
+    return this._name;
+  }
+  get department(){
+    return this._department;
+  }
+  get remainingVacationDays(){
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----------Method Calls------------
+
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+const halley = new Dog('Halley');
+
+let nikko = new Dog('Nikko'); // Create dog named Nikko
+nikko.incrementBehavior(); // Add 1 to nikko instance's behavior
+let bradford = new Dog('Bradford'); // Create dog name Bradford
+console.log(nikko.behavior); // Logs 1 to the console
+console.log(bradford.behavior); // Logs 0 to the console
 
 */
 
